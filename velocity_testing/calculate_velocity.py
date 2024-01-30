609f210ead5a8e130972ee28d92ef71c2a16ce2c
Müller Nicolai <nicolai.mueller@univie.ac.at>
2024-01-30 09:29:42 +0100

    Added initial guess; citation

--- velocity_testing/calculate_velocity.py
+++ velocity_testing/calculate_velocity.py
@@ -5,6 +5,7 @@ sys.path.append('/home/nico/VSCodeRepos/SigMA')
 from NoiseRemoval.bulk_velocity_solver import dense_sample, optimize_velocity_skycoords
 from NoiseRemoval.OptimalVelocity import vr_solver, transform_velocity, optimize_velocity
 
+# CITATION: This code is taken directly from SigMA
 def calculate_velocity(
     data, 
     rho,
@@ -34,7 +35,7 @@ def calculate_velocity(
 
     # extract dense core
     cut_dense_core = dense_sample(rho)
-    ra, dec, plx, pmra, pmdec, rv, pmra_err, pmdec_err, rv_err = data.loc[
+    ra, dec, plx, pmra, pmdec, rv, pmra_err, pmdec_err, rv_err = data.iloc[
         cut_dense_core
     ][cols].values.T
 
@@ -43,10 +44,15 @@ def calculate_velocity(
     rv_copy[np.isnan(rv)] = 0
     rv_err_copy = np.copy(rv_err)
     rv_err_copy[np.isnan(rv_err)] = 1e5
+    mean_uvw = np.zeros(3)
+    uvw_cols = ["U", "V", "W"]
+    if uvw_cols is not None:
+        mean_uvw = np.mean(data.iloc[cut_dense_core][uvw_cols], axis=0)
     sol = optimize_velocity(
-        ra, dec, plx, pmra, pmdec, rv_copy, pmra_err, pmdec_err, rv_err_copy
+        ra, dec, plx, pmra, pmdec, rv_copy, pmra_err, pmdec_err, rv_err_copy, init_guess=mean_uvw
     )
     optimal_vel = sol.x[:3]
+    print(optimal_vel)
 
     rv_computed = np.copy(rv)
     rv_isnan_or_large_err = np.isnan(rv_computed)
